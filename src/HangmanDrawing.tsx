45bb52386ee7d57f80d4b81244dbaecd46c8b57a
platisam <platisam@gmail.com>
2023-10-18 11:06:14 +0200

    creating head and body elements of hangman

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+const HEAD = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  ></div>
+);
+
+const BODY = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      background: "black",
+      position: "absolute",
+      top: "120px",
+      right: "0",
+    }}
+  ></div>
+);
 
 interface Props {}
 
 const HangmanDrawing = (props: Props) => {
   return (
     <div style={{ position: "relative" }}>
+      {HEAD}
+      {BODY}
       <div
         style={{
           height: "50px",
