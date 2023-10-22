d8a75e55758b04833c422e6ff7b05eaffac05bcb
platisam <platisam@gmail.com>
2023-10-22 21:45:25 +0200

    completing the keyboard logic

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -32,7 +32,7 @@ const KEYS = [
 type KeyboardProps = {
   activeLetters: string[];
   inactiveLetters: string[];
-  addGuessLetter: (letter: string) => void;
+  addGuessedLetter: (letter: string) => void;
 };
 
 export function Keyboard({
@@ -49,10 +49,15 @@ export function Keyboard({
       }}
     >
       {KEYS.map((key) => {
+        const isActive = activeLetters.includes(key);
+        const isInactive = inactiveLetters.includes(key);
         return (
           <button
             onClick={() => addGuessedLetter(key)}
-            className={`${styles.btn}`}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${
+              isInactive ? styles.inactive : ""
+            }`}
+            disabled={isInactive || isActive}
             key={key}
           >
             {key}
